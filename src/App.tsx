58d15aff588ec4e7e71a1c563b6ce664bf8171ac
hatoma daiya <phaosdove@gmail.com>
2024-12-06 17:31:38 +0900

    adminの一覧ページを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Index from './pages/Index'
 import AdminPdfDetails from './pages/admin/pdf/[id]/details'
+import AdminPdfIndex from './pages/admin/pdf/List'
 import Header from './components/Header'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/admin/pdf/:id/details" element={<AdminPdfDetails />} />
+          <Route path="/admin" element={<AdminPdfIndex />} />
         </Routes>
       </div>
     </Router>
